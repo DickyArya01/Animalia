7cba23cdb78deec1d403049403e49b1e66198ce2
dickyarya <dickyarya170101@gmail.com>
2022-04-30 16:20:28 +0700

    modify AddButton.cs

---
+++ Assets/Script/AddButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButton : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+    
+    void Awake() {
+
+    }
+}
