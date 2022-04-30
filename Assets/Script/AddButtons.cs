7d21cb7c1a0663d853a54f431a7804b783521705
dickyarya <dickyarya170101@gmail.com>
2022-04-30 16:39:05 +0700

    add loop to Instantiate button in awake function

--- Assets/Script/AddButtons.cs
+++ Assets/Script/AddButtons.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddButton : MonoBehaviour
+public class AddButtons : MonoBehaviour
 {
     [SerializeField]
     private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject puzzleButton;
     
     void Awake() {
-
+        for (int i=0; i < 8; i++) {
+            GameObject button = Instantiate(puzzleButton);
+            button.name = "Button" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
     }
 }
