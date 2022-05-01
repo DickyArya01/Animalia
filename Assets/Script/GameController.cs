b76e99798a8d2afe276f52e4a0bf09e40cb8a1ef
dickyarya <dickyarya170101@gmail.com>
2022-05-01 19:45:24 +0700

    menambah fungsi generate gambar puzzle, menambah fungsi gamepuzzle dan menambah resource gambar

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -8,13 +8,31 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private Sprite bgImage;
 
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
     public List<Button> buttons = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses, countCorrectGuesses, gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessName, secondGuessName;
 
     void Start() {
         
         GetButton();
+        AddListeners();
+        AddGamePuzzles();
 
     } 
+
+    void Awake() {
+        puzzles = Resources.LoadAll<Sprite> ("Sprites/animals");
+    }
 
     void GetButton() {
 
@@ -24,6 +42,31 @@ public class GameController : MonoBehaviour
             buttons.Add(objects[i].GetComponent<Button>());
             buttons[i].image.sprite = bgImage;
         }
+    }
+
+    void AddGamePuzzles() {
+        int looper = buttons.Count;
+        int index = 0;
+
+        for (int i=0; i < looper; i++) {
+            if(index == looper /2) {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+
+            index++;
+        }
+    }
+
+    void AddListeners() {
+        foreach (Button button in buttons) {
+            button.onClick.AddListener(() => PickUpPuzzle());
+        }
+    }
+
+    public void PickUpPuzzle() {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log(name +" is clicked");
     }
 
 }
